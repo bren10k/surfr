55b697abeff6ebf1937fc7eeea15bb519bc3da9c
bren10k <mauibrenton@gmail.com>
2017-01-30 15:16:55 -1000

    Update app.py

--- app.py
+++ app.py
@@ -57,10 +57,11 @@ def makeWebhookResult(data):
 
     # print(json.dumps(item, indent=4))
 
- #   speech = "Today in " + data.get('spot_name') + ": " + data.get('avg_max_size') + \
-  #           ", waves are " + data.get('spot_id')
+    speech = "Today in " + data.get('spot_name')
+    #" + data.get('avg_max_size') + \
+   #          ", waves are " + data.get('spot_id')
  
-    speech = "Hello there"
+    #speech = "Hello there"
    
 
     print("Response:")
